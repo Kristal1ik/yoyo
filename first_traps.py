6e7922709df68f536c1904610d194cf9e4d254a4
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-02-15 15:09:07 +0300

    Update first_traps.py

--- first_traps.py
+++ first_traps.py
@@ -1,9 +1,10 @@
 from remade_functions import Controller, intersection, Trapezoid, \
-    find_min_point, func, area, Rules, making_rules
+    find_min_point, func, area, Rules
 import matplotlib.pyplot as plt
 
+
 if __name__ == '__main__':
-    trap = Controller([[6, 1], [-9, 1]])
+    trap = Controller([[6, 1], [4, 1]])
     point_x = intersection(trap.x[0], trap.x[1], Rules.rule_x[0], Rules.rule_x[1])
     plt.subplot(331)
     plt.plot(trap.x[0], trap.x[1], 'b', Rules.rule_x[0], Rules.rule_x[1], 'r')
@@ -15,9 +16,9 @@ if __name__ == '__main__':
     plt.scatter(point_v[0], point_v[1], color="#7B68EE")
 
     plt.subplot(333)
-    trunc1 = Trapezoid([Rules.w_1, find_min_point(point_x, point_v)])
+    trunc1 = Trapezoid([Rules.w1, find_min_point(point_x, point_v)])
     trunc1.trapezoid()
-    plt.plot(Rules.rule_w[0], trunc1.lst_y, 'g', Rules.rule_w[0], Rules.rule_w[1])
+    plt.plot(Rules.rule_w1[0], trunc1.lst_y, 'g', Rules.rule_w1[0], Rules.rule_w1[1])
 
     trap2 = Controller([[3, 1], [6, 1]])
 
@@ -32,13 +33,13 @@ if __name__ == '__main__':
     plt.scatter(point_v2[0], point_v2[1], color="#FF69B4")
 
     plt.subplot(336)
-    trunc2 = Trapezoid([Rules.w_2, find_min_point(point_x2, point_v2)])
+    trunc2 = Trapezoid([Rules.w2, find_min_point(point_x2, point_v2)])
     trunc2.trapezoid()
-    plt.plot(Rules.rule_w_2[0], Rules.rule_w_2[1], trunc2.lst_x, trunc2.lst_y, 'g')
+    plt.plot(Rules.rule_w2[0], Rules.rule_w2[1], trunc2.lst_x, trunc2.lst_y, 'g')
 
     plt.subplot(337)
     plt.scatter(0, 1, color="#7B68EE", alpha=0)
-    plt.plot(Rules.rule_w[0], trunc1.lst_y, 'g')
+    plt.plot(Rules.rule_w1[0], trunc1.lst_y, 'g')
 
     plt.subplot(338)
     plt.scatter(0, 1, color="#7B68EE", alpha=0)
