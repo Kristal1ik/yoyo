6180d5cacbfc7c21c6c63d357424a53386c20b5e
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-02-18 10:00:46 +0300

    it doesn't work(

--- cycle.py
+++ cycle.py
@@ -8,7 +8,7 @@ maxis = 0.01
 r = 0.002
 R = 0.03
 dt = 0.01
-T = 50
+T = 30
 x0 = 0.05
 v0 = 0
 l = 0.5
@@ -30,7 +30,7 @@ def f(x, v, w):
         xnew = l
     if xnew < R:
         xnew = R
-    print(f"xnew = {xnew} vnew = {vnew}")
+    # print(f"xnew = {xnew} vnew = {vnew}")
 
     return xnew, vnew
 
@@ -44,18 +44,37 @@ if __name__ == '__main__':
         lst = []
         trap = Controller([[x, 1], [v, 1]])
 
-        point_x = intersection(trap.x[0], trap.x[1], Rules.rule_x[0], Rules.rule_x[1])
-        point_v = intersection(trap.v[0], trap.v[1], Rules.rule_v[0], Rules.rule_v[1])
-        trunc1 = Trapezoid([Rules.w1, find_min_point(point_x, point_v)])
+        point_x = intersection(trap.x[0], trap.x[1], Rules.x1[0], Rules.x1[1])
+        point_v = intersection(trap.v[0], trap.v[1], Rules.v1[0], Rules.v1[1])
+        trunc1 = Trapezoid([Rules.w1[0], Rules.w1[1], Rules.w1[2], Rules.w1[3], find_min_point(point_x, point_v)])
         trunc1.trapezoid()
 
-        point_x2 = intersection(trap.x[0], trap.x[1], Rules.rule_x2[0], Rules.rule_x2[1])
-        point_v2 = intersection(trap.v[0], trap.v[1], Rules.rule_v2[0], Rules.rule_v2[1])
-        trunc2 = Trapezoid([Rules.w2, find_min_point(point_x2, point_v2)])
+        point_x2 = intersection(trap.x[0], trap.x[1], Rules.x2[0], Rules.x2[1])
+        point_v2 = intersection(trap.v[0], trap.v[1], Rules.v2[0], Rules.v2[1])
+        trunc2 = Trapezoid([Rules.w2[0], Rules.w2[1], Rules.w2[2], Rules.w2[3], find_min_point(point_x2, point_v2)])
         trunc2.trapezoid()
+
+        point_x3 = intersection(trap.x[0], trap.x[1], Rules.x3[0], Rules.x3[1])
+        point_v3 = intersection(trap.v[0], trap.v[1], Rules.v3[0], Rules.v3[1])
+        trunc3 = Trapezoid([Rules.w3[0], Rules.w3[1], Rules.w3[2], Rules.w3[3], find_min_point(point_x3, point_v3)])
+        trunc3.trapezoid()
+
+        point_x4 = intersection(trap.x[0], trap.x[1], Rules.x4[0], Rules.x4[1])
+        point_v4 = intersection(trap.v[0], trap.v[1], Rules.v4[0], Rules.v4[1])
+        trunc4 = Trapezoid([Rules.w4[0], Rules.w4[1], Rules.w4[1], Rules.w4[3], find_min_point(point_x4, point_v4)])
+        trunc4.trapezoid()
+
+        point_x5 = intersection(trap.x[0], trap.x[1], Rules.x5[0], Rules.x5[1])
+        point_v5 = intersection(trap.v[0], trap.v[1], Rules.v5[0], Rules.v5[1])
+        trunc5 = Trapezoid([Rules.w5[0], Rules.w5[1], Rules.w5[2], Rules.w5[3], find_min_point(point_x5, point_v5)])
+        trunc5.trapezoid()
 
         lst.append(trunc1)
         lst.append(trunc2)
+        lst.append(trunc3)
+        lst.append(trunc4)
+        lst.append(trunc5)
+
         w = func(lst)
         w = area(w)
         x, v = f(x, v, w)
