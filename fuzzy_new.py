94c60c40bf0c23200e04da1ff5cd773351737def
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-03-20 09:29:38 +0300

    fuzzy

--- fuzzy_new.py
+++ fuzzy_new.py
@@ -2,10 +2,10 @@ class Trapezoid:
     def __init__(self, params):
         if len(params) == 2:
             n = params[0]
-            self.a = n - 2
-            self.b = n - 1
-            self.c = n + 1
-            self.d = n + 2
+            self.a = n - 0.4
+            self.b = n - 0.2
+            self.c = n + 0.2
+            self.d = n + 0.4
             self.h = params[1]
         elif len(params) == 5:
             self.a = params[0]
@@ -13,6 +13,12 @@ class Trapezoid:
             self.c = params[2]
             self.d = params[3]
             self.h = params[4]
+        elif len(params) == 6:
+            self.a = params[0][0] - params[1]
+            self.b = params[0][0] - params[2]
+            self.c = params[0][0] + params[3]
+            self.d = params[0][0] + params[4]
+            self.h = 1
         self.lst_y = []
         self.lst_x = []
         self.lst_h = []
@@ -53,8 +59,8 @@ class Trapezoid:
 
 class Controller:
     def __init__(self, params):
-        self.x = Trapezoid(params[0]).trapezoid()
-        self.v = Trapezoid(params[1]).trapezoid()
+        self.x = Trapezoid([params[0], 0.05, 0.008, 0.008, 0.019, 0]).trapezoid()
+        self.v = Trapezoid([params[1], 0.071, 0.0575, 0.0575, 0.15, 0]).trapezoid()
         self.y1 = min([self.intersection(self.x.a, self.x.b, self.x.c, self.x.d,
                                          Rules.x1.a, Rules.x1.b, Rules.x1.c, Rules.x1.d),
                        self.intersection(self.v.a, self.v.b, self.v.c, self.v.d,
@@ -111,41 +117,76 @@ class Controller:
     def return_(self):
         return self.y1, self.y2, self.y3, self.y4, self.y5
 
-
 class Rules:
-    x1 = Trapezoid([0.09, 0.14, 0.156, 0.175, 1])
+    x1 = Trapezoid([9.000e-02,  1.400e-01,  1.560e-01,  1.750e-01, 1])
     x1.trapezoid()
-    v1 = Trapezoid([0.280, 0.351, 0.466, 0.616, 1])
+    v1 = Trapezoid([2.800e-01,  3.510e-01, 4.660e-01,  6.160e-01, 1])
     v1.trapezoid()
-    w1 = [6.00, 9.550, 13.550, 16.350, 1]
+    w1 = [6.000e+00,  9.550e+00,  1.355e+01, 1.635e+01, 1]
 
     # 2 правило
-    x2 = Trapezoid([0.270, 0.320, 0.336, 0.355, 1])
+    x2 = Trapezoid([2.700e-01, 3.200e-01, 3.360e-01, 3.550e-01, 1])
     x2.trapezoid()
-    v2 = Trapezoid([-0.09, -0.019, 0.096, 0.246, 1])
+    v2 = Trapezoid([-9.000e-02, -1.900e-02, 9.600e-02,  2.460e-01, 1])
     v2.trapezoid()
-    w2 = [14.000, 17.550, 21.550, 24.350, 1]
+    w2 = [1.400e+01,  1.755e+01,  2.155e+01,  2.435e+01, 1]
 
     # 3 правило
-    x3 = Trapezoid([0.010, 0.060, 0.076, 0.095, 1])
+    x3 = Trapezoid([1.000e-02, 6.000e-02,  7.600e-02,  9.500e-02, 1])
     x3.trapezoid()
-    v3 = Trapezoid([0.280, 0.351, 0.466, 0.616, 1])
+    v3 = Trapezoid([2.800e-01,  3.510e-01, 4.660e-01,  6.160e-01, 1])
     v3.trapezoid()
-    w3 = [6.000, 9.550, 13.550, 16.350, 1]
+    w3 = [6.000e+00,  9.550e+00,  1.355e+01,  1.635e+01, 1]
+
 
     # 4 правило
-    x4 = Trapezoid([0.480, 0.530, 0.546, 0.565, 1])
+    x4 = Trapezoid([4.800e-01,  5.300e-01,  5.460e-01,  5.650e-01, 1])
     x4.trapezoid()
-    v4 = Trapezoid([0.730, 0.801, 0.916, 1.066, 1])
+    v4 = Trapezoid([7.300e-01,  8.010e-01, 9.160e-01,  1.066e+00, 1])
     v4.trapezoid()
-    w4 = [20.000, 23.550, 27.550, 30.350, 1]
+    w4 = [2.000e+01,  2.355e+01,  2.755e+01,  3.035e+01, 1]
 
     # 5 правило
-    x5 = Trapezoid([0.270, 0.320, 0.336, 0.355, 1])
+    x5 = Trapezoid([2.700e-01,  3.200e-01,  3.360e-01,  3.550e-01, 1])
     x5.trapezoid()
-    v5 = Trapezoid([-0.090, -0.019, 0.096, 0.246, 1])
+    v5 = Trapezoid([-9.000e-02, -1.900e-02, 9.600e-02,  2.460e-01, 1])
     v5.trapezoid()
-    w5 = [20.000, 23.550, 27.550, 30.3500, 1]
+    w5 = [2.000e+01,  2.355e+01,  2.755e+01,  3.035e+01, 1]
+# class Rules:
+#     x1 = Trapezoid([0.09, 0.14, 0.156, 0.175, 1])
+#     x1.trapezoid()
+#     v1 = Trapezoid([0.280, 0.351, 0.466, 0.616, 1])
+#     v1.trapezoid()
+#     w1 = [6.00, 9.550, 13.550, 16.350, 1]
+#
+#     # 2 правило
+#     x2 = Trapezoid([0.270, 0.320, 0.336, 0.355, 1])
+#     x2.trapezoid()
+#     v2 = Trapezoid([-0.09, -0.019, 0.096, 0.246, 1])
+#     v2.trapezoid()
+#     w2 = [14.000, 17.550, 21.550, 24.350, 1]
+#
+#     # 3 правило
+#     x3 = Trapezoid([0.010, 0.060, 0.076, 0.095, 1])
+#     x3.trapezoid()
+#     v3 = Trapezoid([0.280, 0.351, 0.466, 0.616, 1])
+#     v3.trapezoid()
+#     w3 = [6.000, 9.550, 13.550, 16.350, 1]
+#
+#
+#     # 4 правило
+#     x4 = Trapezoid([0.480, 0.530, 0.546, 0.565, 1])
+#     x4.trapezoid()
+#     v4 = Trapezoid([0.730, 0.801, 0.916, 1.066, 1])
+#     v4.trapezoid()
+#     w4 = [20.000, 23.550, 27.550, 30.350, 1]
+#
+#     # 5 правило
+#     x5 = Trapezoid([0.270, 0.320, 0.336, 0.355, 1])
+#     x5.trapezoid()
+#     v5 = Trapezoid([-0.090, -0.019, 0.096, 0.246, 1])
+#     v5.trapezoid()
+#     w5 = [20.000, 23.550, 27.550, 30.3500, 1]
 
 
 def func(lst_m):
@@ -177,4 +218,18 @@ def area(lst_w):
     except ZeroDivisionError:
         return 0
     return integr1 / integr2
-
+# 4.18146640e-01 -1.79497512e-01  8.70269649e-02  3.03605818e-02
+#  -4.16378341e-01 -7.02737886e-02 -1.64044628e-01 -1.19823955e-01
+#   4.13621014e+00  6.54102338e+00  9.84232245e+00  1.40241447e+01
+#  -2.50078058e-01 -6.12273210e-03  2.85736156e-01 -4.67160251e-02
+#   2.77949506e-01 -1.22649861e-01  1.62101339e-01  4.21372881e-01
+#   7.83600279e-01  2.34639689e+00  5.78330644e+00  1.59383212e+01
+#  -2.73647487e-01  4.42686194e-01 -1.17028443e-01  1.13545814e-01
+#  -4.97731466e-01  3.85085132e-01 -2.33416664e-01  2.35434833e-01
+#   7.17782751e-01  1.08587348e+00  6.84135410e+00  1.08201027e+01
+#   1.06229987e-01 -2.17359783e-02  1.21964888e-01  5.31392736e-01
+#   4.18709429e-01 -3.02583499e-01  4.51610717e-01 -2.40911794e-01
+#   9.31468738e-01  3.06607083e+00  1.28034116e+01  1.92613045e+01
+#  -4.08816955e-01  3.17998779e-01  4.83229377e-01  6.91891148e-01
+#   1.93770182e-01 -1.15794876e-01 -3.73195570e-01  4.69588296e-01
+#   3.13099570e+00  5.77091427e+00  1.02507205e+01  1.31139935e+01
